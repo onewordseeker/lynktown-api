a3ecc466f27d248ba679aff7abf157aa09d4612f
Hamid Raza <englishking786@gmail.com>
2023-05-23 13:06:11 +0500

    asd

--- app/Http/Controllers/OrderRequestController.php
+++ app/Http/Controllers/OrderRequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\OrderRequest;
 use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
@@ -16,44 +17,10 @@ class OrderController extends Controller
         if (!$store) {
             return $this->error('Please complete your business profile.', 401);
         }
-        $orders = Order::where(['store_id' => $store->id])->get();
+        $orders = OrderRequest::where(['store_id' => $store->id])->get();
         return $this->success([
             $orders
         ]);
-    }
-
-    // Create a new store
-    public function store(Request $request)
-    {
-        // Validate the request data
-        $validatedData = Validator::make($request->all(), [
-            'total_price' => 'nullable|string',
-            'shipping_address' => 'nullable|string',
-            'shipping_charges' => 'nullable|string',
-            'order_start_date' => 'nullable|string',
-            'customer_name' => 'nullable|string',
-            'phone_no' => 'nullable|string',
-            'note' => 'nullable|string',
-            'status' => 'nullable',
-            'order_id' => 'required',
-        ]);
-
-        if ($validatedData->fails()) {
-            $message = $validatedData->errors()->first();
-            return $this->error($message, 401);
-        }
-        // Create a new Store model instance
-        $order = new Order;
-        
-        foreach ($validatedData->validated() as $key => $value) {
-            $order->$key = $value;
-        }
-
-        // Save the new Store instance to the database
-        $order->save();
-        return $this->success([
-            $order
-        ], 'Order was created successfully');
     }
 
     // Update an existing store
@@ -66,15 +33,10 @@ class OrderController extends Controller
         }
         // Validate the request data
         $validatedData = Validator::make($request->all(), [
-            'total_price' => 'nullable|string',
-            'shipping_address' => 'nullable|string',
-            'shipping_charges' => 'nullable|string',
-            'order_start_date' => 'nullable|string',
-            'customer_name' => 'nullable|string',
+            'full_name' => 'nullable|string',
             'phone_no' => 'nullable|string',
+            'status' => 'nullable|string',
             'note' => 'nullable|string',
-            'status' => 'nullable',
-            'order_id' => 'required',
         ]);
         if ($validatedData->fails()) {
             $message = $validatedData->errors()->first();
@@ -85,13 +47,13 @@ class OrderController extends Controller
         // $this->OTPMiddleware(null, $request);
 
         // Create a new Store model instance
-        $order = Order::find($request->order_id);
+        $order = OrderRequest::find($request->order_id);
         $order->update($validatedData->validated());
         // Save the new Store instance to the database
         $order->save();
         return $this->success([
             $order
-        ], 'Store was updated successfully');
+        ], 'Order Request was updated successfully');
     }
 
     // Get a single store by ID
@@ -102,7 +64,7 @@ class OrderController extends Controller
         if (!$store) {
             return $this->error('Please complete your business profile.', 401);
         }
-        $store = Order::find($request->order_id);
+        $store = OrderRequest::find($request->order_id);
         return $this->success([
             $store
         ]);
